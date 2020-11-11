1014f0c2eea29cc05129d3cf435a2fa316663eca
Patrick McCormick <patrick.m.mccormick@intel.com>
2020-11-11 15:27:33 -0800

    Simple search script.

---
+++ search.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TMPFILE=/mnt/ramdisk/stdout.cell
+MAX=0
+
+CMPXZ="xz -c -z -9"
+CMPGZ="gzip -c -9 -f"
+
+function div() { echo "scale=3 ; $1 / $2" | bc; }
+
+function fsize() { stat -c %s $1; }
+
+for i in $(seq 0 $1); do
+	# 2**64 - 1
+	#RULEN=$(shuf -i 1-18446744073709551615 -n 1)
+	RULEN=$(shuf -i 1-7625597484987 -n 1)
+	#RULEN=$i
+	CMD="./target/release/ca1d 3 3 $RULEN @ --output=Raw --to=400 --width=400"
+	$CMD > $TMPFILE
+	SZ=$($CMPGZ $TMPFILE | wc -c)
+
+	if (( ${SZ} >= ${MAX} )); then
+		echo -n $(div $SZ $(fsize $TMPFILE))
+		echo -e " : ${CMD}"
+		MAX=${SZ}
+	fi
+
+done
